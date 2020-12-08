d0ba39662e508c3bd32ef953c8951d17e1fcae1f
unknown <ahmostafaa94@gmail.com>
2020-12-08 14:31:38 +0200

    update code

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -9,6 +9,6 @@ export class NewsService {
   constructor(public _http:HttpClient) { }
   getNews(c):Observable<any>
   {
-   return this._http.get(`http://newsapi.org/v2/top-headlines?country=${c}&apiKey=10109dca40ea4bf7bca08305f2b8a1d8`);
+   return this._http.get(`https://newsapi.org/v2/top-headlines?country=${c}&apiKey=10109dca40ea4bf7bca08305f2b8a1d8`);
  }
 }
